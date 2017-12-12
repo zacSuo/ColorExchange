cfb07b45a9b1a08499ee8e0caaff93e8279abd25
zac <suoxd123@126.com>
2017-12-12 09:36:35 +0800

    rgb 2 hsv

---
+++ rgb2hsv/winForm/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace winForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbG = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbH = new System.Windows.Forms.Label();
+            this.lbS = new System.Windows.Forms.Label();
+            this.lbV = new System.Windows.Forms.Label();
+            this.btnTran = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbR
+            // 
+            this.tbR.Location = new System.Drawing.Point(94, 25);
+            this.tbR.Name = "tbR";
+            this.tbR.Size = new System.Drawing.Size(100, 25);
+            this.tbR.TabIndex = 1;
+            this.tbR.Tag = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "R";
+            // 
+            // tbG
+            // 
+            this.tbG.Location = new System.Drawing.Point(94, 56);
+            this.tbG.Name = "tbG";
+            this.tbG.Size = new System.Drawing.Size(100, 25);
+            this.tbG.TabIndex = 2;
+            this.tbG.Tag = "2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "G";
+            // 
+            // tbB
+            // 
+            this.tbB.Location = new System.Drawing.Point(94, 87);
+            this.tbB.Name = "tbB";
+            this.tbB.Size = new System.Drawing.Size(100, 25);
+            this.tbB.TabIndex = 3;
+            this.tbB.Tag = "3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(262, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "H";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(262, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 15);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "S";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(262, 90);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 15);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "V";
+            // 
+            // lbH
+            // 
+            this.lbH.AutoSize = true;
+            this.lbH.Location = new System.Drawing.Point(296, 28);
+            this.lbH.Name = "lbH";
+            this.lbH.Size = new System.Drawing.Size(0, 15);
+            this.lbH.TabIndex = 1;
+            // 
+            // lbS
+            // 
+            this.lbS.AutoSize = true;
+            this.lbS.Location = new System.Drawing.Point(296, 59);
+            this.lbS.Name = "lbS";
+            this.lbS.Size = new System.Drawing.Size(0, 15);
+            this.lbS.TabIndex = 1;
+            // 
+            // lbV
+            // 
+            this.lbV.AutoSize = true;
+            this.lbV.Location = new System.Drawing.Point(296, 90);
+            this.lbV.Name = "lbV";
+            this.lbV.Size = new System.Drawing.Size(0, 15);
+            this.lbV.TabIndex = 1;
+            // 
+            // btnTran
+            // 
+            this.btnTran.Location = new System.Drawing.Point(154, 129);
+            this.btnTran.Name = "btnTran";
+            this.btnTran.Size = new System.Drawing.Size(142, 33);
+            this.btnTran.TabIndex = 4;
+            this.btnTran.Text = "转换";
+            this.btnTran.UseVisualStyleBackColor = true;
+            this.btnTran.Click += new System.EventHandler(this.btnTran_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 174);
+            this.Controls.Add(this.btnTran);
+            this.Controls.Add(this.lbV);
+            this.Controls.Add(this.lbS);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbH);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbB);
+            this.Controls.Add(this.tbG);
+            this.Controls.Add(this.tbR);
+            this.Name = "Form1";
+            this.Text = "RGB转换为HSV";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbG;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbH;
+        private System.Windows.Forms.Label lbS;
+        private System.Windows.Forms.Label lbV;
+        private System.Windows.Forms.Button btnTran;
+    }
+}
+
---
+++ rgb2hsv/winForm/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTran_Click(object sender, EventArgs e)
+        {
+            int r = int.Parse(tbR.Text);
+            int g = int.Parse(tbG.Text);
+            int b = int.Parse(tbB.Text);
+            int min = Math.Min(r, Math.Min(g, b));
+            int max = Math.Max(r, Math.Max(g, b));
+
+            int h=0, s, v;
+            if (max == min)
+                h = 0;
+            else if (max == r && g >= b)
+                h = 60 * (g - b) / (max - min);
+            else if (max == r && g < b)
+                h = 60 * (g - b) / (max - min) + 360;
+            else if (max == g)
+                h = 60 * (b - r) / (max - min) + 120;
+            else if (max == b)
+                h = 60 * (r - g) / (max - min) + 240;
+
+            if (max == 0)
+                s = 0;
+            else
+                s = (max - min) / max;
+
+            v = max;
+
+            lbH.Text = h.ToString();
+            lbS.Text = s.ToString();
+            lbV.Text = v.ToString();
+        }
+    }
+}
